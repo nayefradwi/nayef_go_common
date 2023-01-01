a9592acc7e1d717ff8c03bc79c8bd1807bfcf357
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-01-01 14:12:33 +0300

    improved parsebody

--- response/handler.go
+++ response/handler.go
@@ -11,8 +11,9 @@ import (
 
 type successCallback[T any] func(T)
 
-func ParseBody[T any](w http.ResponseWriter, body io.ReadCloser, data T, onSuccess successCallback[T]) {
-	err := json.NewDecoder(body).Decode(data)
+func ParseBody[T any](w http.ResponseWriter, body io.ReadCloser, onSuccess successCallback[T]) {
+	var data T
+	err := json.NewDecoder(body).Decode(&data)
 	if err != nil {
 		log.Printf("failed to parse body: %s", err.Error())
 		WriteErrorResponse(w, baseError.NewInternalServerError().(*baseError.BaseError))
