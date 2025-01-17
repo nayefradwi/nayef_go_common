b42f3ccad28b6b14a620653a7086bf6e9fda05eb
naifradwi <naif.mazen.radwi@gmail.com>
2025-01-17 11:23:04 +0300

    created base logger logic

--- go.mod
+++ go.mod
@@ -17,4 +17,5 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
--- modules/logging/logger.go
+++ modules/logging/logger.go
@@ -1,1 +1,60 @@
 package logging
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var defaultRollingLogger = &lumberjack.Logger{
+	Filename:   "server.log",
+	MaxSize:    500,
+	MaxBackups: 3,
+	MaxAge:     30,
+}
+
+func ReplaceDefaultRollingLogger(logger *lumberjack.Logger) {
+	defaultRollingLogger = logger
+}
+
+func GetDefaultRollingLogger() *lumberjack.Logger {
+	return defaultRollingLogger
+}
+
+func ReplaceDefaultFileName(filename string) {
+	defaultRollingLogger.Filename = filename
+}
+
+func NewRollingLogger(filename string, maxSize int, maxBackups int, maxAge int) *lumberjack.Logger {
+	return &lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    maxSize,
+		MaxBackups: maxBackups,
+		MaxAge:     maxAge,
+	}
+}
+
+type LoggerConfig struct {
+	Syncs     []zapcore.WriteSyncer
+	InfoLevel zapcore.Level
+	Encoder   zapcore.Encoder
+}
+
+func NewLoggerConfig(syncs []zapcore.WriteSyncer, infoLevel zapcore.Level, encoder zapcore.Encoder) LoggerConfig {
+	return LoggerConfig{
+		Syncs:     syncs,
+		InfoLevel: infoLevel,
+		Encoder:   encoder,
+	}
+}
+
+func (c LoggerConfig) ReplaceGlobalLogger() {
+	cores := make([]zapcore.Core, len(c.Syncs))
+	for i, sync := range c.Syncs {
+		cores[i] = zapcore.NewCore(c.Encoder, sync, c.InfoLevel)
+	}
+
+	core := zapcore.NewTee(cores...)
+	logger := zap.New(core)
+	zap.ReplaceGlobals(logger)
+}
