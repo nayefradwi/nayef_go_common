231bfc0cce9117656dfecd8855ce585715160816
naifradwi <naif.mazen.radwi@gmail.com>
2025-01-23 16:47:55 +0300

    Deleted old implementation cuz AI generation sucks

--- modules/cache/store.go
+++ modules/cache/store.go
@@ -4,15 +4,3 @@ type InMemoryCacheParams struct {
 	MaxSize         int
 	CachingStrategy int
 }
-
-func NewInMemoryCacheStore(params InMemoryCacheParams) ICacheStore {
-	switch params.CachingStrategy {
-	case FifoCache:
-		return NewFifoCacheStore(params)
-	case LifoCache:
-		return NewLifoCacheStore(params)
-	default:
-		// TODO: change to LruCache
-		return NewFifoCacheStore(params)
-	}
-}
