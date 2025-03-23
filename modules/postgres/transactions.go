665ae51742d2141e62552eed6b4a8c7730aa1300
naif radwi <naif.mazen.radwi@gmail.com>
2025-03-23 14:58:25 +0300

    implemented transaction with data to avoid early returns when transaction hasnt fully been committed

--- modules/postgres/transactions.go
+++ modules/postgres/transactions.go
@@ -9,7 +9,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func WithTx(ctx context.Context, pool *pgxpool.Pool, f func(ctx context.Context, tx pgx.Tx) error) error {
+func Tx(ctx context.Context, pool *pgxpool.Pool, f func(ctx context.Context, tx pgx.Tx) error) error {
 	tx, err := pool.Begin(ctx)
 	if err != nil {
 		zap.L().Error("Error starting transaction", zap.Error(err))
@@ -27,4 +27,26 @@ func WithTx(ctx context.Context, pool *pgxpool.Pool, f func(ctx context.Context,
 	}
 
 	return nil
+}
+
+func TxWithData[T any](ctx context.Context, pool *pgxpool.Pool, f func(ctx context.Context, tx pgx.Tx) (T, error)) (T, error) {
+	var empty T
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		zap.L().Error("Error starting transaction", zap.Error(err))
+		return empty, core.InternalError("Error starting transaction")
+	}
+	defer tx.Rollback(ctx)
+	data, err := f(ctx, tx)
+	if err != nil {
+		zap.L().Error("Error executing transaction", zap.Error(err))
+		return data, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		zap.L().Error("Error committing transaction", zap.Error(err))
+		return empty, core.InternalError("Error committing transaction")
+	}
+
+	return data, nil
 }
