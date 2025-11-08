dcab2f60c4740f81b82298094ea23946272d4e52
naif radwi <naif.mazen.radwi@gmail.com>
2025-11-08 13:16:08 +0300

    fixing recoverer for grpc

--- modules/grpc/recover.go
+++ modules/grpc/recover.go
@@ -2,31 +2,34 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 func RecoverUnary() grpc.UnaryServerInterceptor {
-	return recoverInterceptor
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (resp any, err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = handleRecoveredPanic(r)
+			}
+		}()
+		return handler(ctx, req)
+	}
 }
 
-func recoverInterceptor(
-	ctx context.Context,
-	req any,
-	info *grpc.UnaryServerInfo,
-	handler grpc.UnaryHandler,
-) (resp any, err error) {
-	defer func() { err = recoverError() }()
-	return handler(ctx, req)
-}
-
-func recoverError() error {
-	recovered := recover()
-	if err, ok := recovered.(error); ok {
+func handleRecoveredPanic(r any) error {
+	if err, ok := r.(error); ok {
 		zap.L().Error("internal server error", zap.Any("error", err), zap.Stack("stack trace"))
 		return NewGrpcResponseWriter[any]().WriteError(err)
 	}
 
-	return nil
+	zap.L().Error("internal server error", zap.Any("error", r), zap.Stack("stack trace"))
+	return NewGrpcResponseWriter[any]().WriteError(fmt.Errorf("%v", r))
 }
