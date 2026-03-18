46da629e3d6ae1a2bf87b82dc0316f6c20099b1c
naif radwi <naif.mazen.radwi@gmail.com>
2026-03-18 18:59:41 +0300

    fixed pagination

--- pagination/go.mod
+++ pagination/go.mod
@@ -1,3 +1,7 @@
 module github.com/nayefradwi/nayef_go_common/pagination
 
-go 1.25.0
+go 1.26.0
+
+require github.com/nayefradwi/nayef_go_common/httputil v1.0.1
+
+require github.com/nayefradwi/nayef_go_common/errors v1.0.1 // indirect
--- pagination/offset.middleware.go
+++ pagination/offset.middleware.go
@@ -2,12 +2,14 @@ package pagination
 
 import (
 	"net/http"
+
+	"github.com/nayefradwi/nayef_go_common/httputil"
 )
 
 func SetOffsetPaginationMiddleware(f http.Handler) http.Handler {
 	hanlder := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		pageKey := GetIntQueryParam(r, PageKey)
-		pageSizeKey := GetIntQueryParam(r, PageSizeKey)
+		pageKey := httputil.GetIntQueryParam(r, PageKey)
+		pageSizeKey := httputil.GetIntQueryParam(r, PageSizeKey)
 		query := NewOffsetPageQuery(pageKey, pageSizeKey)
 
 		ctx := query.WithContext(r.Context())
