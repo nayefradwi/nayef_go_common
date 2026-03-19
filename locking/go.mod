923a569f3c3f2de441248f625c279c4c02d82b16
naif radwi <naif.mazen.radwi@gmail.com>
2026-03-19 14:27:51 +0300

    migrated locking

---
+++ locking/go.mod
@@ -0,0 +1,18 @@
+module github.com/nayefradwi/nayef_go_common/locking
+
+go 1.26.0
+
+require (
+	github.com/go-redsync/redsync/v4 v4.16.0
+	github.com/nayefradwi/nayef_go_common/errors v1.0.1
+	github.com/nayefradwi/nayef_go_common/redisutil v0.1.1
+	github.com/redis/go-redis/v9 v9.18.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
--- locking/service.go
+++ locking/service.go
@@ -6,9 +6,8 @@ import (
 
 	"github.com/go-redsync/redsync/v4"
 	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
-	modules_locking "github.com/nayefradwi/nayef_go_common/modules/locking"
-	modules_redis "github.com/nayefradwi/nayef_go_common/modules/redis"
-	"github.com/nayefradwi/nayef_go_common/result"
+	"github.com/nayefradwi/nayef_go_common/errors"
+	"github.com/nayefradwi/nayef_go_common/redisutil"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -16,28 +15,27 @@ type DistributedLocker struct {
 	rs *redsync.Redsync
 }
 
-func NewDistributedLocker(rs *redsync.Redsync) modules_locking.ILocker {
+func NewDistributedLocker(rs *redsync.Redsync) ILocker {
 	return &DistributedLocker{
 		rs: rs,
 	}
 }
 
-func NewDistributedLockerFromClient(client *redis.Client) modules_locking.ILocker {
+func NewDistributedLockerFromClient(client *redis.Client) ILocker {
 	pool := goredis.NewPool(client)
 	rs := redsync.New(pool)
 	return NewDistributedLocker(rs)
 }
 
-func NewDistributedLockerFromConnection(ctx context.Context, connection string) modules_locking.ILocker {
-	config := modules_redis.NewConnectionConfig(connection)
-	client := config.Connect(ctx)
+func NewDistributedLockerFromConnection(ctx context.Context, connection string) ILocker {
+	client := redisutil.ConnectToRedis(ctx, connection)
 	return NewDistributedLockerFromClient(client)
 }
 
 func (l *DistributedLocker) AcquireLock(
 	ctx context.Context,
 	key string,
-	params modules_locking.LockParams,
+	params LockParams,
 ) error {
 	mutex := l.rs.NewMutex(
 		key,
@@ -48,7 +46,7 @@ func (l *DistributedLocker) AcquireLock(
 
 	time.Sleep(params.InitialWaitTime)
 	if err := mutex.Lock(); err != nil {
-		return result.BadRequestError("failed to acquire lock")
+		return errors.BadRequestError("failed to acquire lock")
 	}
 
 	return nil
@@ -57,7 +55,7 @@ func (l *DistributedLocker) AcquireLock(
 func (l *DistributedLocker) AcquireLocks(
 	ctx context.Context,
 	keys []string,
-	params modules_locking.LockParams,
+	params LockParams,
 ) error {
 	for _, key := range keys {
 		err := l.AcquireLock(ctx, key, params)
@@ -84,7 +82,7 @@ func (l *DistributedLocker) ReleaseLocks(ctx context.Context, keys []string) {
 func (l *DistributedLocker) RunWithLock(
 	ctx context.Context,
 	key string,
-	params modules_locking.LockParams,
+	params LockParams,
 	f func() error,
 ) error {
 	err := l.AcquireLock(ctx, key, params)
@@ -99,7 +97,7 @@ func (l *DistributedLocker) RunWithLock(
 func (l *DistributedLocker) RunWithLocks(
 	ctx context.Context,
 	keys []string,
-	params modules_locking.LockParams,
+	params LockParams,
 	f func() error,
 ) error {
 	err := l.AcquireLocks(ctx, keys, params)
