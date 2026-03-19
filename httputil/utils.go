66b436a7f282c653f7a2a987f713ceac11b97705
naif radwi <naif.mazen.radwi@gmail.com>
2026-03-19 13:43:52 +0300

    added get bearer token

--- httputil/utils.go
+++ httputil/utils.go
@@ -3,10 +3,19 @@ package httputil
 import (
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 func GetIntQueryParam(r *http.Request, key string) int {
 	query := r.URL.Query().Get(key)
 	value, _ := strconv.Atoi(query)
 	return value
+}
+
+func GetBearerToken(r *http.Request) string {
+	header := r.Header.Get("Authorization")
+	if !strings.HasPrefix(header, "Bearer ") {
+		return ""
+	}
+	return strings.TrimPrefix(header, "Bearer ")
 }
